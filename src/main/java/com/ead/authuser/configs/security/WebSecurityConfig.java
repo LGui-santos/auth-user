57930c6fa3ac86e87b0dc9f96115ce4341c61e18
Guilherme <guilhermesantos.srs@gmail.com>
2026-09-03 17:36:41 -0300

    Implementar UserDetails e UserDetailsService para Basic Auth com Database.

--- src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -2,16 +2,13 @@ package com.ead.authuser.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,24 +20,24 @@ public class WebSecurityConfig {
             "/auth/**"
     };
 
+    private final AuthenticationEntryPointImpl authenticationEntryPoint;
+
+    public WebSecurityConfig(AuthenticationEntryPointImpl authenticationEntryPoint) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
                 .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(authenticationEntryPoint))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(AUTH_WHITELIST).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService  userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder().encode("123456"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(admin);
     }
 
     @Bean
